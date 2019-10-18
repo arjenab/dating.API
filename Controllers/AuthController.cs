b59ee54945ea7bd9419537f6b6fc8059f38a5bb8
Arjen <Arjen.ab@hybrit.org>
2019-10-18 15:47:07 +0200

    signin working

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,19 +17,19 @@ namespace DatingApp.API.Controllers
 
         }
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
             // Validate request
 
-            username = username.ToLower();
-            if(await _Repo.UserExists(username))
+            userForRegisterDto.Username =  userForRegisterDto.Username.ToLower();
+            if(await _Repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("User exists");
             var userToCreate = new User
             {
-                UserName = username
+                UserName = userForRegisterDto.Username
             };
 
-            var createdUser = await _Repo.Register(userToCreate, password);
+            var createdUser = await _Repo.Register(userToCreate, userForRegisterDto.Password);
             return StatusCode(201);
         }
     }

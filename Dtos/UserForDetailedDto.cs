3e4082c6b82379db077cac56b458c29c54e33ad7
Arjen <Arjen.ab@hybrit.org>
2019-11-19 15:01:46 +0100

    dto for extending api

--- Dtos/UserForDetailedDto.cs
+++ Dtos/UserForDetailedDto.cs
@@ -1,16 +1,15 @@
 using System;
 using System.Collections.Generic;
+using DatingApp.API.Models;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Dtos
 {
-    public class User
+    public class UserForDetailedDto
     {
-        public int id { get; set; }
+            public int id { get; set; }
         public string UserName { get; set; }
-        public byte[] passwordHash { get; set; }
-        public byte[] passwordSalt { get; set; }
         public string Gender { get; set; }
-        public DateTime DateofBirth{ get; set; }
+        public int Age{ get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
@@ -18,6 +17,7 @@ namespace DatingApp.API.Models
         public string LookingFor { get; set; }
         public string Interest { get; set; }
         public string City { get; set; }
-        public ICollection<Photo> Photos { get; set; }
+        public string PhotoUrl { get; set; }
+        public ICollection<PhotosForDetailedDto> Photos { get; set; }
     }
 }
--- Dtos/UserForListDto.cs
+++ Dtos/UserForListDto.cs
@@ -1,23 +1,19 @@
 using System;
-using System.Collections.Generic;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Dtos
 {
-    public class User
+    public class UserForListDto
     {
         public int id { get; set; }
         public string UserName { get; set; }
-        public byte[] passwordHash { get; set; }
-        public byte[] passwordSalt { get; set; }
         public string Gender { get; set; }
-        public DateTime DateofBirth{ get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
-        public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interest { get; set; }
         public string City { get; set; }
-        public ICollection<Photo> Photos { get; set; }
+        public string PhotoUrl { get; set; }
     }
 }
